87d1f6acae17b03210468e0b4f8dd60f4bff236e
JC <james.calingo@gmail.com>
2020-01-10 11:45:49 -0500

    adding games in

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="header display-3">JavaScript Arcade!</div>
+      <p>I like games. I've programmed quite a few of them.</p>
     </div>
   );
 }
