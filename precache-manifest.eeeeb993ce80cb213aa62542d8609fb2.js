e8e42177992dd662708032133d6c600ed4b4411c
JC <james.calingo@gmail.com>
2020-01-13 16:57:41 -0500

    Updates

---
+++ precache-manifest.eeeeb993ce80cb213aa62542d8609fb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2225e3d9608b7e4047bf85a7a98696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c5f505f21cefe11a2ea",
+    "url": "/static/css/main.7c48b0e5.chunk.css"
+  },
+  {
+    "revision": "24db4213e8c4732a0663",
+    "url": "/static/js/2.6d84165b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6d84165b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c5f505f21cefe11a2ea",
+    "url": "/static/js/main.e2edf242.chunk.js"
+  },
+  {
+    "revision": "a83ad97455c3a388f4f1",
+    "url": "/static/js/runtime-main.fb2ffba5.js"
+  }
+]);
