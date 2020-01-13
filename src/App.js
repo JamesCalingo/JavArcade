440ae5d92684424698bba8d4c2a43d6f7b0d5cc6
JC <james.calingo@gmail.com>
2020-01-13 16:51:29 -0500

    css

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ function App() {
   return (
     <div className="App">
       <div className="header display-3">JavaScript Arcade!</div>
-      <p>I like games. I've programmed quite a few of them.</p>
+      <p>I like games. I've programmed quite a few of them and am constantly looking for more games to do.</p>
+      <p>I've decided to curate all of those games into one place: this page. You can see all of the games I've made here.
+      </p>
+      <h1 className="mb-4">Enjoy!</h1>
       <CardContainer />
+      <h1>...And more soon</h1>
     </div>
    
   );
--- src/Card.js
+++ src/Card.js
@@ -2,13 +2,15 @@ import React from "react";
 
 function Card(props) {
   return (<div className="card mb-3">
+
     <div className="card-header text-center">
       <h3>{props.name}</h3>
     </div>
     <div className="card-body border-bottom mb-2">
       {props.description}</div>
-    <a className="button btn btn-lg" href={props.link}>{props.button}</a>
-  </div>
+    <a className="button btn btn-lg btn-primary" href={props.link}>{props.button}</a>
+    </div>
+  
   )
 }
 
